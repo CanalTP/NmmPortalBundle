1eed819df8c23f5761a9cc453019314ef32bf351
pthegner <pierre.thegner@canaltp.fr>
2019-06-28 11:59:03 +0200

    Update script

--- Migrations/pdo_pgsql/Version004.php
+++ Migrations/pdo_pgsql/Version004.php
@@ -16,12 +16,12 @@ class Version004 extends AbstractMigration
 
     public function up(Schema $schema)
     {
-        $this->addSql('CREATE TABLE t_language (lang_id INT NOT NULL, lang_code VARCHAR(2) NOT NULL, PRIMARY KEY(lang_id));');
-        $this->addSql("INSERT INTO t_language (lang_id, lang_code) VALUES (1, 'fr'), (2, 'en');");
-        $this->addSql('ALTER TABLE tr_customer_cus ADD COLUMN cus_lang INT NOT NULL DEFAULT 1;');
-        $this->addSql('ALTER TABLE tr_customer_cus ADD FOREIGN KEY(cus_lang) REFERENCES t_language(lang_id) ON DELETE NO ACTION;');
-        $this->addSql('ALTER TABLE t_user_usr ADD COLUMN usr_lang INT DEFAULT NULL;');
-        $this->addSql('ALTER TABLE t_user_usr ADD FOREIGN KEY(usr_lang) REFERENCES t_language(lang_id) ON DELETE SET NULL;');
+        $this->addSql('CREATE TABLE t_language (lang_code VARCHAR(2) NOT NULL, lang_label VARCHAR(255) NOT NULL, PRIMARY KEY(lang_code));');
+        $this->addSql("INSERT INTO t_language (lang_code, lang_label) VALUES ('fr', 'français'), ('en', 'english');");
+        $this->addSql("ALTER TABLE tr_customer_cus ADD COLUMN cus_lang VARCHAR(2) NOT NULL DEFAULT 'fr';");
+        $this->addSql('ALTER TABLE tr_customer_cus ADD FOREIGN KEY(cus_lang) REFERENCES t_language(lang_code) ON DELETE NO ACTION;');
+        $this->addSql('ALTER TABLE t_user_usr ADD COLUMN usr_lang VARCHAR(2) DEFAULT NULL;');
+        $this->addSql('ALTER TABLE t_user_usr ADD FOREIGN KEY(usr_lang) REFERENCES t_language(lang_code) ON DELETE SET NULL;');
     }
 
     public function down(Schema $schema)
